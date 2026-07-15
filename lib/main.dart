daa7bdc34030ce450272afde679b38550c31a05c
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-07-15 00:58:58 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'theme.dart';
-import 'screens/login_screen.dart';
 import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/groups_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/messages_screen.dart';
 import 'screens/notifications_screen.dart';
-import 'screens/course_selection_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,24 +29,9 @@ class SmartStudyBuddy extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const SplashScreen(),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-          if (snapshot.hasData) {
-            return const MainNavigation();
-          }
-          return const LoginScreen();
-        },
-      ),
     );
   }
 }
-
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
