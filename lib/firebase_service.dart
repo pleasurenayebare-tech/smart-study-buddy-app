d8122ec5267ddb9d5480e40fe51c410f44a4def7
annanambi10-commits <annanambi10@gmail.com>
2026-07-25 04:44:22 -0700

    Add import for Uint8List in firebase_service.dart

--- lib/firebase_service.dart
+++ lib/firebase_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:typed_data';
 
 class FirebaseService {
   // Auth instance
